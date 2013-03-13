a07c15c1bb559ea3008abaf47681f9f039fc9737
Adi Sayoga <adisayoga@gmail.com>
2013-03-13 13:43:31 +0800

    Ganti file libs dengan versi lebih bari

--- app/js/boot.js
+++ app/js/boot.js
@@ -1,8 +1,8 @@
 require.config({
   paths: {
-    'jquery': '/libs/jquery',
-    'underscore': '/libs/underscore',
-    'backbone': '/libs/backbone',
+    'jquery': '/libs/jquery.min',
+    'underscore': '/libs/underscore.min',
+    'backbone': '/libs/backbone.min',
     'text': '/libs/text',
     'templates': '../templates'
   },
